bffcd94a57881c73f3bbcf95da5fa472e62d43a1
Tushar Singla <singlatushar07@gmail.com>
2021-06-18 23:36:47 +0530

    Added some utility functions

---
+++ utils.py
@@ -0,0 +1,67 @@
+MAX_WEIGHT = 400000
+
+
+def parse_csv():
+    with open("sample.csv", 'r') as file:
+        next(file)
+        mempool = {}
+        for line in file.readlines():
+            args = line.strip().split(',')
+            tx_id = args[0]
+            fees = args[1]
+            weight = args[2]
+            parents = args[3].split(';')
+            if(parents[0] == ''):
+                parents = []
+            transaction = {
+                "tx_id": tx_id,
+                "fees": fees,
+                "weight": weight,
+                "parents": parents
+            }
+            mempool[tx_id] = transaction
+
+        return mempool
+
+
+def read_block():
+    transactions = []
+    with open("block.txt", "r") as file:
+        for tx in file.readlines():
+            transactions.append(tx.strip())
+        return transactions
+
+
+def ifValidBlock(mempool, blockTransactions):
+    tx_included = {}
+    for tx_id in mempool.keys():
+        tx_included[tx_id] = False
+
+    total_fees = 0
+    total_weight = 0
+
+    for tx_id in blockTransactions:
+        if tx_id not in mempool:
+            print("Transaction {} not in mempool".format(tx_id))
+            return False
+
+        transaction = mempool[tx_id]
+        total_fees += transaction["fees"]
+        total_weight += transaction["weight"]
+        if(total_weight > MAX_WEIGHT):
+            print("Weight of block exceeds required limit")
+            return False
+
+        parents = transaction["parents"]
+        if(len(parents) > 0):
+            for parent in parents:
+                if(tx_included[parent] == False):
+                    print(
+                        "Parent {} fn transaction {} not present in the block".format(parent, tx_id))
+                    return False
+
+        tx_included[tx_id] = True
+
+    print("Block is correct with transaction fees {} and total weight {}".format(
+        total_fees, total_weight))
+    return True
